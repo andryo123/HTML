97fc617594486ce4365534ccaa53971b0ef85475
Andrybienfe <93975662+Andrybienfe@users.noreply.github.com>
2022-09-27 10:11:20 +0300

    te

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,26 @@
 /*Mettee içi votre code JAVASCRIPT*/
-$(document).ready(function(){
-    $(window).scroll(function(){
+function onScrollInit( items, trigger ) {
+  items.each( function() {
+    var osElement = $(this),
+        osAnimationClass = osElement.attr('data-os-animation'),
+        osAnimationDelay = osElement.attr('data-os-animation-delay');
+      
+        osElement.css({
+          '-webkit-animation-delay':  osAnimationDelay,
+          '-moz-animation-delay':     osAnimationDelay,
+          'animation-delay':          osAnimationDelay
+        });
+
+        var osTrigger = ( trigger ) ? trigger : osElement;
         
-      });
-})
+        osTrigger.waypoint(function() {
+          osElement.addClass('animated').toggleClass(osAnimationClass);
+          },{
+              triggerOnce: false,
+              offset: '90%'
+        });
+  });
+}
+
+ onScrollInit( $('.os-animation') );
+ onScrollInit( $('.staggered-animation'), $('.staggered-animation-container') );
